46db935a4a2274f5e843c256c99f103da6755f2c
jovicdevelopment <josephjovic23@gmail.com>
2026-06-15 14:07:02 +0300

    style: update footer branding to jovictech development team

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,16 +10,16 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
+          {emoji("Made with ❤️ by jovictech development team")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a
-            href="https://github.com/saadpasta/developerFolio"
+            href="https://github.com/jovicdevelopment/jovicdevelopment.github.io"
             target="_blank"
             rel="noreferrer"
           >
-            developerFolio
+            jovictech development
           </a>
         </p>
       </div>
